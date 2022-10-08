65fb82b2dc8ce0318c89b82a8537f61268b1c372
Ajay Singh <104777198+ajay226022@users.noreply.github.com>
2022-10-08 11:20:36 -0700

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,23 @@
+<?php
+if(!isset($_SESSION)){
+  session_start();
+}
+// session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = 'learn_php';
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$db);
+
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+// echo "Connected successfully";
+
+
+
+?>
